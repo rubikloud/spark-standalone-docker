59436c6c6e138850341ec556052385c0da6281cb
Tw UxTLi51Nus <TwUxTLi51Nus@posteo.de>
2016-09-24 23:08:39 +0200

    Changed daemon launch commands to run in foreground

--- launch.sh
+++ launch.sh
@@ -12,20 +12,15 @@ else
     
     # Start Master
     if [ "$1" == "master" ]; then
-        # if using w/ 'exec', script terminates after completion of
-        # this command, the rest of the script is not executed (BUG?)
-        # put exec in front of command
-        "${SPARK_HOME}/sbin/start-master.sh"
+
+        exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.master.Master"
 
     # Start Slave
     elif [ "$1" == "slave" ]; then
 
         if [ -n "$2" ]; then
 
-            # if using w/ 'exec', script terminates after completion of
-            # this command, the rest of the script is not executed (BUG?)
-            # put exec in front of command
-            "${SPARK_HOME}/sbin/start-slave.sh" "${2}"
+            exec "${SPARK_HOME}/bin/spark-class" "org.apache.spark.deploy.worker.Worker" "${2}"
 
         else
             
@@ -41,8 +36,5 @@ else
         exit 1
 
     fi
-
-    # view logfile of started service:
-    exec tail -f ${SPARK_HOME}/logs/*
 
 fi
